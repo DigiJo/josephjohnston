e9f375317d415158311c4805583253aaa1bb2e59
Joe Johnston <joejohnston1989@gmail.com>
2018-10-04 20:11:41 +0100

    Add image styling and changed h2 to h1 for blog posts

--- portfolio/.vuepress/components/JournalList.vue
+++ portfolio/.vuepress/components/JournalList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="journal-list">
     <div v-for="post in journal" :key="post.title" class="post">
-      <router-link tag="h2" :to="post.path" class="title">{{ post.frontmatter.title }}</router-link>
+      <router-link tag="h1" :to="post.path" class="title">{{ post.frontmatter.title }}</router-link>
       <p>{{ post.frontmatter.excerpt }}</p>
-      <p>({ post.frontmatter.thumbnail })</p>
+      <div class="post">({ post.frontmatter.thumbnail })</div>
     </div>
   </div>
 </template>
@@ -31,14 +31,24 @@
   .post:last-of-type {
     border: 0;
   }
-  .post h2 {
+  .post h1 {
     margin: 0 0 1rem 0;
   }
-  .post h2:hover {
+  .post h1:hover {
     opacity: 0.6;
   }
   .post p {
     margin: 0;
     color: #333;
+  }
+    .post {
+    position: relative;
+    width: 100%;
+    height: 70vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin-bottom: 5vw;
+    cursor: pointer;
   }
 </style>
